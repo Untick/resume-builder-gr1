84f48df8c61df9208919aba86ed267bc3fd808c0
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-09-06 19:26:05 +0300

    New app and other updates

---
+++ Avilin Mark/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegistrationView, LoginView
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('update/<int:user_id>/', RegistrationView.as_view(), name='update_user'),
+    path('delete/<int:user_id>/', RegistrationView.as_view(), name='delete_user'),
+]
