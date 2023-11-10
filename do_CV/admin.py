db7d96c60c9c853ce8bc22924001b360e6f3d4f0
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-11-10 12:51:32 +0200

    Delete do_CV directory

--- do_CV/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from do_CV.models import DoCV
-
-admin.site.Register(DoCV)
--- do_CV/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_cv/', views.create_cv, name='create_cv'),
-    path('update_cv', views.update_cv, name='update_cv'),
-]
