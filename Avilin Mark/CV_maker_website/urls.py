64818c087b6d46b5ac2c2fd95128e31e5d87feaa
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-11-10 12:45:53 +0200

    Delete Avilin Mark/CV_maker_website directory

--- Avilin Mark/CV_maker_website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('order/', include('do_order.urls')),
-    path('order/', include('users.urls'))
-]
