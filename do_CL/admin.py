790c80c4c8fafeb031b22ca180cfa4a9e5a437cd
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-11-10 12:51:00 +0200

    Delete do_CL directory

--- do_CL/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from do_CL.models import DoCL
-
-admin.site.Register(DoCL)
--- do_CL/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_cl/', views.create_cl, name='create_cl'),
-    path('update_cl', views.update_cl, name='update_cl'),
-]
