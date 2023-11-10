f67d58adc17d2a4962df3df07cbbc763510b44c9
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-11-10 12:51:20 +0200

    Delete do_order directory

--- do_order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import OrderCVCreateView, OrderCVUpdateView
-
-urlpatterns = [
-    path('cv-order/', OrderCVCreateView.as_view(), name='order_cv_create'),
-    path('update-cv-order/<int:pk>/', OrderCVUpdateView.as_view(), name='order_cv_update'),
-    path('cl-order/', OrderCVCreateView.as_view(), name='order_cl_create'),
-    path('update-cl-order/<int:pk>/', OrderCVUpdateView.as_view(), name='order_cl_update'),
-]
